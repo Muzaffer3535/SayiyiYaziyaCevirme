4bdbdc2fba350e39f62a42cd707931a7f024c7f0
Muzaffer3535 <113903806+Muzaffer3535@users.noreply.github.com>
2023-01-03 14:05:10 +0300

    SayiyiYaziyaCevirme
    Odev2

---
+++ SayıyıYazıyaCevirme/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayıyıYazıyaCevirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc = 0, gsonuc = 0;
+            short a;
+            string s = "";
+            string[] Birler = { "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz" };
+            string[] Onlar = { "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan" };
+            string[] Yüzler = { "Yüz", "İkiyüz", "Üçyüz", "Dörtyüz", "Beşyüz", "Altıyüz", "Yediyüz", "Sekizyüz", "Dokuzyüz" };
+            Console.Write("Bir sayı giriniz: ");
+            a = short.Parse(Console.ReadLine());
+            if (a / 100 >= 1)
+            {
+                s = Yüzler[(a / 100) - 1];
+            }
+            sonuc = (a / 100);
+            sonuc = sonuc * 100;
+            gsonuc = a - sonuc;
+
+            if (gsonuc / 10 >= 1)
+            {
+                s = s + Onlar[(gsonuc / 10) - 1];
+            }
+            if (gsonuc % 10 != 0)
+            {
+                s = s + Birler[(gsonuc % 10) - 1];
+            }
+            Console.WriteLine(s);
+            Console.ReadKey(true);
+        }
+    }
+}
